847a9900d9bfef5c3f85159893c26c87b677a549
Robert Przybylski <56112893+przybylskirobert@users.noreply.github.com>
2020-12-01 13:51:40 +0100

    Add files via upload

---
+++ VMSS_GoldenImage_configuration_steps.ps1
@@ -0,0 +1,34 @@
+$verboseSettings = $VerbosePreference
+$VerbosePreference = 'Continue'
+$toolsPath = "C:\Tools"
+$agentURL = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"
+$agentMSI = "Microsoft.RDInfra.RDAgent.Installer-x64-1.0.2548.6500.msi"
+$agentMSIPath = "$toolsPath\$agentMSI"
+$bootLoaderURL = "https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH"
+$bootLoaderMSI = "Microsoft.RDInfra.RDAgentBootLoader.Installer-x64.msi"
+$bootLoaderMSIPath = "$toolsPath\$bootLoaderMSI"
+
+
+$toolsTest = Test-Path -Path $toolsPath
+if ($toolsTest -eq $false){
+    Write-Verbose "Creating '$toolsPath' directory"
+    New-Item -ItemType Directory -Path $toolsPath | Out-Null
+}
+else {
+    Write-Verbose "Directory '$toolsPath' already exists."
+}
+Write-Verbose "Downloading '$agentURL' into '$agentMSIPath '"
+Invoke-WebRequest -Uri $agentURL -OutFile $agentMSIPath 
+Write-Verbose "Downloading '$bootLoaderURL' into '$bootLoaderMSIPath'"
+Invoke-WebRequest -Uri $bootLoaderURL -OutFile $bootLoaderMSIPath
+
+Set-Location "$toolsPath"
+. .\$agentMSI
+Start-Sleep -Seconds 60
+. .\$bootLoaderMSI
+
+Push-Location "$env:SystemRoot\system32\sysprep"
+. .\sysprep.exe
+Pop-Location 
+
+$VerbosePreference = $verboseSettings 
